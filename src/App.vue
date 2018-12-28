2474678d33971ef0dfbcabeac89b1d4a7fc69f5b
coltborg <coltborg@gmail.com>
2018-12-27 19:14:29 -0600

    Add tailwind and add basic setup
    Closes #4
    Closes #5

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <RouterLink to="/">
-        Home
-      </RouterLink> |
-      <RouterLink to="/about">
-        About
-      </RouterLink>
-    </div>
     <RouterView />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@tailwind preflight;
+@tailwind components;
+@tailwind utilities;
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,58 @@
 <template>
-  <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="h-screen bg-grey-lighter">
+    <h1 class="pt-10 pb-4 text-center text-5xl text-black">
+      Visual Timer
+    </h1>
+    <h2 class="pb-4 text-center text-xl font-normal text-grey-darker">
+      Is there a timer running? - {{ isTimerRunning }}
+    </h2>
+    <h2 class="pb-6 text-center text-xl font-normal text-grey-darker">
+      The start time of the timer is - {{ displayTime }}
+    </h2>
+    <div class="text-center">
+      <button
+        v-show="!isTimerRunning"
+        class="px-4 py-2 bg-purple text-white rounded hover:bg-purple-dark"
+        type="button"
+        @click="handleStartTime">
+        Start
+      </button>
+      <button
+        v-show="isTimerRunning"
+        class="px-4 py-2 bg-red text-white rounded hover:bg-red-dark"
+        type="button"
+        @click="handleStopTime">
+        Stop
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import format from 'date-fns/format';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      isTimerRunning: false,
+      startTime: null,
+    };
+  },
+  computed: {
+    displayTime() {
+      return format(this.startTime, 'h:mm:ss a');
+    },
+  },
+  methods: {
+    handleStartTime() {
+      this.startTime = new Date();
+      this.isTimerRunning = true;
+    },
+    handleStopTime() {
+      this.startTime = null;
+      this.isTimerRunning = false;
+    },
   },
 };
 </script>
