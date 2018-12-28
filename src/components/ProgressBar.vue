e4bb154f4890d4188c3c35282dc7c4b36d60e3fa
coltborg <coltborg@gmail.com>
2018-12-27 23:06:23 -0600

    Add start timer functionality and visualization

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <svg
+    width="36"
+    height="36"
+    class="flip rounded-full"
+    viewBox="0 0 36 36">
+    <path
+      class="background"
+      :d="circlePath" />
+    <path
+      class="bar"
+      :d="circlePath"
+      :stroke-dasharray="dashArrayValue" />
+  </svg>
+</template>
+
+<style scoped>
+.flip {
+  transform: scale(-1, 1);
+}
+
+.background {
+  fill: none;
+  stroke: var(--path-color, #DAE1E7);
+  stroke-width: var(--path-stroke, 32);
+}
+.bar {
+  fill: none;
+  stroke: var(--bar-stroke, #9561E2);
+  stroke-width: var(--path-stroke, 32);
+  transition: stroke 1s;
+}
+</style>
+
+<script>
+export default {
+  name: 'ProgressBar',
+  props: {
+    percentage: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      circlePath:
+        'M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831',
+    };
+  },
+  computed: {
+    dashArrayValue() {
+      return `${this.percentage}, 100`;
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="h-screen bg-grey-lighter">
+  <div class="min-h-screen bg-grey-lighter">
     <h1 class="pt-10 pb-4 text-center text-5xl text-black">
       Visual Timer
     </h1>
+    <div class="text-center">
+      <ProgressBar
+        :percentage="timeRemainingPercentage"
+        class="w-64 h-64" />
+    </div>
+    <h2 class="pb-4 text-center text-xl font-normal text-grey-darker">
+      The time now - {{ now | formatToTime }}
+    </h2>
     <h2 class="pb-4 text-center text-xl font-normal text-grey-darker">
       Is there a timer running? - {{ isTimerRunning }}
     </h2>
     <h2 class="pb-6 text-center text-xl font-normal text-grey-darker">
-      The start time of the timer is - {{ displayTime }}
+      The start time of the timer is - {{ startTime | formatToTime }}
+    </h2>
+    <h2 class="pb-6 text-center text-xl font-normal text-grey-darker">
+      The end time of the timer is - {{ finishTime | formatToTime }}
+    </h2>
+    <h2 class="pb-6 text-red text-center text-xl font-normal text-grey-darker">
+      Time remaining - {{ displayTimeRemaining }}
     </h2>
     <div class="text-center">
       <button
@@ -29,28 +43,72 @@
 </template>
 
 <script>
-import format from 'date-fns/format';
+import datefns from 'date-fns';
+import { ProgressBar } from '@/components';
 
 export default {
   name: 'Home',
+  components: {
+    ProgressBar,
+  },
+  filters: {
+    formatToTime(value) {
+      return datefns.format(value, 'h:mm:ssa');
+    },
+  },
   data() {
     return {
+      finishTime: null,
+      interval: 25,
       isTimerRunning: false,
+      now: new Date(),
       startTime: null,
     };
   },
   computed: {
-    displayTime() {
-      return format(this.startTime, 'h:mm:ss a');
+    timeRemaining() {
+      if (this.isTimerRunning) {
+        return datefns.differenceInSeconds(this.finishTime, this.now);
+      }
+      return 0;
     },
+    displayTimeRemaining() {
+      if (this.isTimerRunning) {
+        return datefns.distanceInWords(
+          this.finishTime,
+          this.now,
+        );
+      }
+      return `${this.interval} ${this.interval > 1 ? 'minutes' : 'minute'}`;
+    },
+    timeRemainingPercentage() {
+      return (this.timeRemaining / (this.interval * 60)) * 100;
+    },
+  },
+  watch: {
+    timeRemaining(newValue) {
+      if (newValue <= 0) {
+        this.handleStopTime();
+      }
+    },
+  },
+  created() {
+    this.nowFunc = () => this.now = new Date();
+    this.nowInterval = setInterval(this.nowFunc, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.nowInterval);
   },
   methods: {
     handleStartTime() {
-      this.startTime = new Date();
+      this.startTime = this.now;
+      // This controls the timer amount
+      this.finishTime = datefns.addMinutes(this.now, this.interval);
       this.isTimerRunning = true;
     },
     handleStopTime() {
       this.startTime = null;
+      this.finishTime = null;
       this.isTimerRunning = false;
     },
   },
