8835afa7f7a306c04b809bb19a9070cab6ddaf55
coltborg <coltborg@gmail.com>
2019-06-15 17:32:43 -0500

    Remove purgecss for now

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,30 @@
-const PurgecssPlugin = require('purgecss-webpack-plugin');
-const glob = require('glob-all');
-const path = require('path');
+// const PurgecssPlugin = require('purgecss-webpack-plugin');
+// const glob = require('glob-all');
+// const path = require('path');
 
-class TailwindExtractor {
-  static extract(content) {
-    return content.match(/[A-Za-z0-9-_:/]+/g) || [];
-  }
-}
+// class TailwindExtractor {
+//   static extract(content) {
+//     return content.match(/[A-Za-z0-9-_:/]+/g) || [];
+//   }
+// }
 
 module.exports = {
-  configureWebpack: {
-    // Merged into the final Webpack config
-    plugins: [
-      new PurgecssPlugin({
-        extractors: [
-          {
-            extractor: TailwindExtractor,
-            extensions: ['html', 'js', 'vue'],
-          },
-        ],
-        paths: glob.sync([
-          path.join(__dirname, './src/index.html'),
-          path.join(__dirname, './**/*.vue'),
-          path.join(__dirname, './src/**/*.js'),
-        ]),
-      }),
-    ],
-  },
+  // configureWebpack: {
+  //   // Merged into the final Webpack config
+  //   plugins: [
+  //     new PurgecssPlugin({
+  //       extractors: [
+  //         {
+  //           extractor: TailwindExtractor,
+  //           extensions: ['html', 'js', 'vue'],
+  //         },
+  //       ],
+  //       paths: glob.sync([
+  //         path.join(__dirname, './src/index.html'),
+  //         path.join(__dirname, './**/*.vue'),
+  //         path.join(__dirname, './src/**/*.js'),
+  //       ]),
+  //     }),
+  //   ],
+  // },
 };
